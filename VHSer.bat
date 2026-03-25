703ceca0198819c80e7b34b139a8db337960a351
Wolf5930 <mateuszsowiski763@gmail.com>
2026-03-25 21:39:35 +0100

    Fix 0 FPS when vhsing a mp4 (by using mediainfo

--- VHSer.bat
+++ VHSer.bat
@@ -2,19 +2,30 @@
 setlocal enabledelayedexpansion
 REM Set FrameSkip to higher/lower to increase/decrease final frame count
 SET /A FrameSkip=10
-REM
-set "input=%~1"
+set "input=%~f1"
 set "folder=%~n1"
 
-for /f "tokens=7" %%a in ('ffmpeg -i "%input%" 2^>^&1 ^| findstr /C:" fps"') do set "original_fps=%%a"
+if /i NOT "%~x1"==".gif" goto regularfps
 
+echo SPECIAL FPS ROUTINE: GIF
+for /f "tokens=7" %%a in ('ffmpeg -i "%input%" 2^>^&1 ^| findstr /C:" fps"') do set "original_fps=%%a"
+goto fpsdone
+
+:regularfps
+echo Regular FPS Routine
+for /f "skip=8 tokens=4" %%a in ('mediainfo "%input%"') do (
+    set "original_fps=%%a"
+    goto stripdecimals
+)
+:stripdecimals
+for /f "tokens=1 delims=." %%a in ("!original_fps!") do set "original_fps=%%a"
+
+:fpsdone
+echo [DEBUG] original_fps = !original_fps!
 mkdir "%folder%"
 cd "%folder%"
-
 set "filter=select=not(mod(n\,!FrameSkip!))"
-
 ffmpeg -i "%input%" -vf "!filter!" -fps_mode vfr frame_%%04d.png
-
 SET /A fps_amount=original_fps / FrameSkip
-
 echo !fps_amount! FPS > anomicinfo.txt
+pause
